17d76274ae0caa58851eff6156e667dcb3800ee7
ruidge <ruidge@qq.com>
2016-01-07 22:14:20 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Ruidge First'});
+    res.render('index', {title: 'ruidge'});
+});
+
+router.get('/blog', function (req, res, next) {
+    res.render('blog', {title: 'blog'});
 });
 
 /* test mysql */
