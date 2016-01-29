b2069cdc9f9118eddb413158ff580ddf6ac40ff9
zhangrui <zhangrui@jijiankang.cn>
2016-01-29 16:52:36 +0800

    fav icon

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/ruidge', function(req, res, next) {
+  res.send('ruidge path');
+});
+
 module.exports = router;
