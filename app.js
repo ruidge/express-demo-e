1553774ebb39bb505b177dc5a68767307719cdca
zhangrui <zhangrui@jijiankang.cn>
2016-06-02 17:00:28 +0800

    add ajax post json data

--- app.js
+++ app.js
@@ -17,13 +17,15 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.urlencoded({ extended: true }));// application/x-www-form-urlencoded 格式key1=value1&key2=value2
+app.use(bodyParser.json());// for parsing application/json head必须有Content-Type: application/json
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);//'/users' is req.baseUrl
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/mock.js
+++ controllers/mock.js
@@ -6,19 +6,21 @@ var mongoose = require('../common/mongoose');
 require('../models/mock');
 var MockModel = mongoose.model('Mock')
 
+//router.post('/addmock', mock.addMock);
 module.exports.addMock = function (req, res, next) {
 
-    var body = req.body.toString();
-    var headers = req.headers;
+    var body = JSON.stringify(req.body);
+    var headers = JSON.stringify(req.headers);
     var method = req.method;
     var url = req.url;
 
     var str = body + "\n" + headers + "\n" + method + "\n" + url;
 
-    console.log(str);
+    console.log(body);
 
-    res.render('testraw', {title: str});
-
+    //res.render('testraw', {title: str});
+    //res.end();
+    res.send(body);
     //var mockModel = new MockModel(body);
     ////
     //mockModel.save(function (err, doc) {
