8e567a709caefd2589049507892c0830dbeff1d8
zhangrui <zhangrui@jijiankang.cn>
2016-06-15 09:44:56 +0800

    commonresult & constants

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -25,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);//'/users' is req.baseUrl
+//app.use('/users', users);//'/users' is req.baseUrl, users is like routes
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/user.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by ruidge on 2016/2/2.
- */
-var mongoose = require('mongoose')
-var User = mongoose.model('User')
