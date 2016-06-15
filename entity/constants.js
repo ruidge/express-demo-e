967c759207bc90931c82fc1fbc9e0f60c551cb3c
zhangrui <zhangrui@jijiankang.cn>
2016-06-15 09:51:14 +0800

    commonresult & constants

---
+++ entity/constants.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lenovo on 2016/6/7.
+ */
+exports.constants = {
+
+    CODE_PATH_ALREADY_EXISTS: 30000,
+    MSG_PATH_ALREADY_EXISTS: 'path路径已经存在'
+
+
+};
---
+++ entity/resultEntity.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ruidge on 2016/6/6.
+ */
+function Result() {
+    this.code = 0;
+    this.successMsg = '';
+    this.errorMsg = '';
+    this.url = '';
+    this.result = null;
+}
+
+exports.Result = Result;
