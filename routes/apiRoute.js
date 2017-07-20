776a30074f8c83e760a1b90740c927eb57f3c0cf
zhangrui <zhangrui@jijiankang.cn>
2017-07-20 17:56:28 +0800

    https

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var mock = require('../api/mock')
+var wechat = require('../api/wechat')
 
 var router = express.Router();
 
@@ -9,5 +10,6 @@ router.post('/addmock', mock.addMock);
 router.get('/getmocks', mock.getMocks);
 //router.get('/getmock/:path', mock.getMockByPath);
 router.get('/mock/:path', mock.mockDataByPath);
+router.get('/wechatTest', wechat.wechatTest);
 
 module.exports = router;
